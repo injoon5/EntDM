0bfe0c147c12404e8839f6c61343a0890159ff7a
thoratica <me@thoratica.net>
2020-08-20 12:02:22 +0900

    Updates

---
+++ precache-manifest.730489cc37d99529b9f1f8d3673ec410.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72c42ca84ef3963f8b6818598628a44",
+    "url": "/EntDM/index.html"
+  },
+  {
+    "revision": "11b8d1899cad47c5df36",
+    "url": "/EntDM/static/css/main.4b784c0e.chunk.css"
+  },
+  {
+    "revision": "f3b4922be54891da3125",
+    "url": "/EntDM/static/js/2.5ad6e3a7.chunk.js"
+  },
+  {
+    "revision": "b26b751f5d83e5c0f8b2fdd2c4b93e2b",
+    "url": "/EntDM/static/js/2.5ad6e3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b8d1899cad47c5df36",
+    "url": "/EntDM/static/js/main.207c9325.chunk.js"
+  },
+  {
+    "revision": "fbe18a5734a5444e64f1",
+    "url": "/EntDM/static/js/runtime-main.a2833b35.js"
+  }
+]);
